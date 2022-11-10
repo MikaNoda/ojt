cd5418512b4a9c0c3bb40da4408ef4fef7d27427
mika noda <mi.p0v0q.ka@gmail.com>
2022-11-10 20:49:11 +0900

    header/footer パーツ化（）

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+        <span>Copyright<small>&copy;</small>Shinohara.ALL RIGHTS RESERVED.</span>
+    </footer>
+
+    <script src="./script.js"></script>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,38 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documentt</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/reset.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-</head>
-
-<body>
-    <header>
-        <div class="header-menu">
-            <ul class="header-li">
-                <li>Menu01</li>
-                <li>Menu02</li>
-                <li>Menu03</li>
-                <li>Menu04</li>
-            </ul>
-        </div>
-        <div class="content">
-            <div class="kv">
-                <img class="kv-logo" src="<?php echo get_template_directory_uri();?>/img/logo.png" alt="logo">
-                <img class="kv-img" src="<?php echo get_template_directory_uri();?>/img/visual_1_pc.png" alt="kv">
-                <img class="sp-img" src="<?php echo get_template_directory_uri();?>/img/visual_1_sp.png" alt="kv">
-                <img class="kv-text" src="<?php echo get_template_directory_uri();?>/img/visual_text_pc.png" alt="text">
-                <img class="sp-text" src="<?php echo get_template_directory_uri();?>/img/visual_text_sp.png" alt="text">
-            </div>
-            <p class="kv-p">進化し続ける「街」アメリカンビレッジマガジン</p>
-            <p class="sp-p">進化し続ける「街」<br>アメリカンビレッジマガジン</p>
-        
-    </header>
+<!-- header読み込み -->
+<?php get_header(); ?>
 
     <main>
         <section class="articles-wrap">
@@ -127,11 +94,5 @@
             </article>
         </section>
     </main>
-    <footer>
-        <span>Copyright<small>&copy;</small>Shinohara.ALL RIGHTS RESERVED.</span>
-    </footer>
-
-    <script src="./script.js"></script>
-</body>
-
-</html>
+   <!-- footer読み込み -->
+   <?php get_footer(); ?>
