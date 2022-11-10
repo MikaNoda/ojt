f88375a675769dc88cee32a9ccc32e2018b365a9
mika noda <mi.p0v0q.ka@gmail.com>
2022-11-10 20:41:19 +0900

    test

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Documentt</title>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/reset.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
